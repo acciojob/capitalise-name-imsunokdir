3c6cecf9f55288eab81d69a4fd8b8c981c9e69a1
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2024-07-15 14:38:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+function capitalizeName(){
+	let inputField  = document.getElementById("fname");
+	inputField.value = inputField.value.toUpperCase();
+}
+document.getElementById("fname").addEventListener('blur', capitalizeName);
+
